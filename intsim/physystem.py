8ba8f30eb9d90306c3533d48c6cf1a704fc557a6
arnau-jr <48213666+arnau-jr@users.noreply.github.com>
2019-10-22 14:26:58 +0200

    Tidied up old UI elements and added new features.
    - Now the particles effective range and visual representation match.
    - Now the box of the physical system is enclosed by infinite walls (velocities flip their sign when touching the edges of the screen).
    - Added energy computation and plotting. After a computation has finished the 'Energy' can be pressed to display a pop-up with a plot of the energies of the system (kinetic, potential and mechanical added over all particles). The total energy of the system should be conserved but it seems that currenty this is not true whenever there is a collision (the energy is still conserved before and after the collision). To dismiss the pop-up, simply click outside of the pop-up window.
    - Added 'Random Lattice' mode for adding particle. It adds N^2 particles (N is the 'lenght' of the lattice) on a square lattice over the space available. It also gives the particles random velocities with a U(-10,10) distribution. 
    - For the time being the attractive part of the LJ potential has been reduced. The particles behave as hard spheres with (almost) no interaction.
    - Upped the computation time to 60 units of time (s in S.I.). 
    - Now the progress of the computation is printed in the console.

--- intsim/physystem.py
+++ intsim/physystem.py
@@ -1,13 +1,16 @@
 
 import numpy as np
 
-def LJ(r):
-    V = 1
-    sig = 5
+def LJ(x,y,param):
+    V = param[0]
+    sig = param[1]
     
-    x = np.where(r < 0.01, 0, 1)*(sig/r)
+    d = np.sqrt(x**2+y**2)
+    mask = np.where(d < 0.000001,1,0)
+    d = d + mask
     
-    value = 4*V*((x)**12-(x)**6)
+    value = 4*V*((sig/d)**12-(sig/d)**6)
+    value = value*(1-mask)
     return value
 
 def dLJx(x,y,param):
@@ -55,7 +58,7 @@ class PhySystem:
     def __init__(self,particles,param):
         self.particles = particles
         self.param = param
-#        self.m = np.vectorize(lambda i: i.m)(particles)
+        self.m = np.vectorize(lambda i: i.m)(particles)
 #        self.q = np.vectorize(lambda i: i.q)(particles)
     def RK4(self,t,dt):
         
@@ -67,6 +70,11 @@ class PhySystem:
         for j in range(0,self.particles.size):
             self.particles[j].r = self.particles[j].r + dt/6 * (k1[j,:2] + 2*k2[j,:2] + 2*k3[j,:2] + k4[j,:2])
             self.particles[j].v = self.particles[j].v + dt/6 * (k1[j,2:] + 2*k2[j,2:] + 2*k3[j,2:] + k4[j,2:])
+            if((np.abs(self.particles[j].r[0])) >= 100):
+                self.particles[j].v[0] = -self.particles[j].v[0]
+            if((np.abs(self.particles[j].r[1])) >= 100):
+                self.particles[j].v[1] = -self.particles[j].v[1]
+        
         return
         
     def f(self,t,delta):
@@ -86,30 +94,62 @@ class PhySystem:
         for j in range(0,N):
             dUx = np.sum(dLJx(dx[j,:],dy[j,:],self.param))
             dUy = np.sum(dLJy(dx[j,:],dy[j,:],self.param))
-#            print(dUx,dUy)
+            
             f[j,:] = np.array([self.particles[j].v[0] + delta[j,2],self.particles[j].v[1] + delta[j,3],-(1/self.particles[j].m)*dUx,-(1/self.particles[j].m)*dUy])
         return f
     
     def solve(self,T,dt):
         t = 0.
-        N = int(T/dt)
+        self.n = int(T/dt)
+        progress = t/T*100
         
-        X = np.vectorize(lambda i: i.r[0])(self.particles)
-        Y = np.vectorize(lambda i: i.r[1])(self.particles)
+        self.X = np.vectorize(lambda i: i.r[0])(self.particles)
+        self.Y = np.vectorize(lambda i: i.r[1])(self.particles)
+        self.VX = np.vectorize(lambda i: i.v[0])(self.particles)
+        self.VY = np.vectorize(lambda i: i.v[1])(self.particles)
+        self.U = np.array([])
         
-        for i in range(0,N):
+        for i in range(0,self.n):
             self.RK4(t,dt)
             t = t + dt
-            X = np.vstack((X,np.vectorize(lambda i: i.r[0])(self.particles)))
-            Y = np.vstack((Y,np.vectorize(lambda i: i.r[1])(self.particles)))
-        return X,Y
+            self.X = np.vstack((self.X,np.vectorize(lambda i: i.r[0])(self.particles)))
+            self.Y = np.vstack((self.Y,np.vectorize(lambda i: i.r[1])(self.particles)))
+            self.VX = np.vstack((self.VX,np.vectorize(lambda i: i.v[0])(self.particles)))
+            self.VY = np.vstack((self.VY,np.vectorize(lambda i: i.v[1])(self.particles)))
+            
+            progress = t/T*100
+            if(progress%10 < 1/30.):
+                print(int(progress),'% done')
+            
+            
+        return
+    
+    def KE(self):
+        self.K = np.sum((self.m*(self.VX**2 + self.VY**2)/2.),axis=1)
+        return
+    def PE(self):
+        self.U = np.zeros([self.n+1])
+        for i in range(0,self.n+1):
+            MX, MXT = np.meshgrid(self.X[i,:],self.X[i,:])
+            MY, MYT = np.meshgrid(self.Y[i,:],self.Y[i,:])
+            
+            dx = MXT - MX
+            dy = MYT - MY
+            u = np.array([])
+            
+            for j in range(0,self.particles.size):
+                u1 = np.sum(LJ(dx[j,:],dy[j,:],self.param))
+                u = np.append(u,np.sum(u1))
+            u = np.sum(u) #Energia potencial total en un instante
+            self.U[i] = u
+        return
 
 #a = particle(1,1,np.array([-3,0]),np.array([0,0]),2)
 #b = particle(1,1,np.array([3,0]),np.array([0,0]),2)
 #c = particle(1,1,np.array([0,1]),np.array([0,1]),2)
 #d = particle(1,1,np.array([3,0]),np.array([1,1]),2)
 #e = particle(1,1,np.array([7,4]),np.array([-1,-1]),2)
-#
+##
 #s = PhySystem(np.array([a,b,c,d,e]))
 #s = PhySystem(np.array([a,b]))
 #print(s.particles[0].r)
