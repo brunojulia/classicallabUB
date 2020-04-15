399421d8f49bc7d9c6e069a7260aad948f18d19c
jofrevalles <61060572+jofrevalles@users.noreply.github.com>
2020-04-15 12:02:41 +0200

    Add files via upload

--- particlesinabox/physystem.py
+++ particlesinabox/physystem.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 #JV: Truncated Lennard-Jones potentials (WCA), see LJverlet function for more explanation and documentation for the expressions.
 #JV: We include both radius as parameters so we can generalize the function to interaction between diferent radius
@@ -7,26 +8,28 @@ def dLJverlet(x,r2,R1,R2,param):
     this only changes when calling the interaction on the algotyhm,
     for all isotrope interactions this should still hold."""
     V = param[0]
-    sig = param[1]
+    sig = param[1] #JV: This is the system units, so we will calculate the new sigma relative to this
     L = param[2]
     rc = (2**(1/6))*((R1+R2)/(2*sig))
+    sig_int = (R1+R2)/(2*sig) #JV: This is the sigma of the interaction (in the system units)
 
     #JV: Because we are working on reduced units (from the values of the Argon gas)
     # we want need to divide our radius by the radius of the Argon gas
 
     #JV: See LJverlet() for more explanation on the truncation
-#    if((r2**(1/2))>rc):
-#        value = 0
-#    else:
-    value = ((48.*x)/(r2))*((((((R1+R2)/(2*sig))**2)*1./r2)**6) - (((((R1+R2)/(2*sig))**2)*0.5/r2)**3))
+    if((r2**(1/2))>rc):
+        value = 0
+    else:
+        value = ((48.*x)/(r2))*(((((sig_int**2)*1.)/r2)**6) - ((((sig_int**2)*0.5)/r2)**3))
 
     return value
 
 def LJverlet(r2,R1,R2,param):
     V = param[0]
-    sig = param[1]
+    sig = param[1] #JV: This is the system units, so we will calculate the new sigma relative to this
     L = param[2]
     rc = (2**(1/6))*((R1+R2)/(2*sig))
+    sig_int = (R1+R2)/(2*sig) #JV: This is the sigma of the interaction (in the system units)
 
     """JV: In doing this, we are actually taking the LJ potential truncated at the minimum potentail eneregy, at a distance
     r=(2**(1/6))*sigma, and we shift upward by the amount of minimum energy on the energy scale so the energy and the force are
@@ -36,7 +39,7 @@ def LJverlet(r2,R1,R2,param):
     if((r2**(1/2))>rc):
         value = 0
     else:
-        value = 4*((((((R1+R2)/(2*sig))**2)*1./r2)**6) - (((((R1+R2)/(2*sig))**2)*1./r2)**3)) - 4*(((((R1+R2)/(2*sig))*1./rc)**12) - ((((R1+R2)/(2*sig)*1./rc)**6)))
+        value = 4*(((((sig_int**2)*1.)/r2)**6) - ((((sig_int**2)*1.)/r2)**3)) + 1
 
     return value
 
@@ -184,16 +187,28 @@ class PhySystem:
     def vel_verlet(self,t,dt,r0,v0,a0):
         """JV: vel_verlet(t,dt,r0,r1,v0,v1) performs one step of the velocity verlet algorithm at
         time t with a step of dt with the previous position r0 and the previous velocity v0, returns
-        the next postion r1 and the next velocity v1"""
+        the next postion r1 and the next velocity v1. If we are on the "In a box" menu, the particles will
+        elastically collide with the walls and in the "Free!" menu, they will go through the walls and appear
+        on the other side."""
         r1 = r0 + v0*dt + 0.5*a0*dt**2 #JV: We calculate x(t+dt)
         a1 = (1/self.m)*np.transpose(self.fv(r1[0,:],r1[1,:],True)) #JV: From x(t+dt) we get a(t+dt)
         v1 = v0 + 0.5*(a0+a1)*dt #JV: From the a(t+dt) and a(t) we get v(t+dt)
 
         L = self.param[2]
 
-        #JV: Border conditions, elastic collision
-        v1[0,:] = np.where((r1[0,:]**2)+(self.R)**2 > (0.495*L)**2,-v1[0,:],v1[0,:])
-        v1[1,:] = np.where((r1[1,:]**2)+(self.R)**2 > (0.495*L)**2,-v1[1,:],v1[1,:])
+#        print(r1)
+#        print("")
+#        print(0.5*L)
+#        sys.exit()
+        if(self.param[3] == "In a box"):
+            #JV: Border conditions, elastic collision
+            v1[0,:] = np.where((r1[0,:]**2)+(self.R)**2 > (0.495*L)**2,-v1[0,:],v1[0,:])
+            v1[1,:] = np.where((r1[1,:]**2)+(self.R)**2 > (0.495*L)**2,-v1[1,:],v1[1,:])
+        elif(self.param[3] == "Free!"):
+            r1[0,:] = np.where(r1[0,:] > 0.5*L,r1[0,:]-1*L,r1[0,:])
+            r1[0,:] = np.where(r1[0,:] < -0.5*L,r1[0,:]+1*L,r1[0,:])
+            r1[1,:] = np.where(r1[1,:] > 0.5*L,r1[1,:]-1*L,r1[1,:])
+            r1[1,:] = np.where(r1[1,:] < -0.5*L,r1[1,:]+1*L,r1[1,:])
 
         return r1[0,:],r1[1,:],v1[0,:],v1[1,:],a1
 
@@ -261,10 +276,32 @@ class PhySystem:
 
         r2 = np.square(dx)+np.square(dy)
 
-        #-np.tile(self.R.reshape(N,1),(1,N)),-np.tile(self.R,(N,1))
-#        r2 = r2-(np.tile(self.R.reshape(N,1),(1,N)))-(np.tile(self.R,(N,1)))
-#
-#        np.fill_diagonal(r2,0)
+        if(self.param[3] == "Free!"):
+        #JV: We do this to get the actual distance in the case of the "Free!" simulation
+#            print(dx)
+#            print("")
+#            print(dy)
+#            print("")
+#            print(r2)
+#            print("")
+            dx_v2 = (abs(dx.copy())-1*L)
+            r2_v2 = dx_v2**2+dy**2
+            dx = np.where(r2 > r2_v2,dx_v2*np.sign(dx),dx)
+            r2 = np.where(r2 > r2_v2,r2_v2,r2)
+            dy_v2 = (abs(dy.copy())-1*L)
+            r2_v2 = dx**2+dy_v2**2
+            dy = np.where(r2 > r2_v2,dy_v2*np.sign(dy),dy)
+            r2 = np.where(r2 > r2_v2,r2_v2,r2)
+            r2_v2 = dx_v2**2+dy_v2**2
+            dx = np.where(r2 > r2_v2,dx_v2*np.sign(dx),dx)
+            dy = np.where(r2 > r2_v2,dy_v2*np.sign(dy),dy)
+            r2 = np.where(r2 > r2_v2,r2_v2,r2)
+#            print(dx)
+#            print("")
+#            print(dy)
+#            print("")
+#            print(r2)
+#            sys.exit()
 
         dUx = 0.
         dUy = 0.
@@ -273,8 +310,12 @@ class PhySystem:
 
         i = len(self.U)
 
-        if((i*self.dt)%1 == 0): #JV: every certain amount of steps we update the list
+        if((i*self.dt)%0.5 == 0): #JV: every certain amount of steps we update the list
             self.close_list = self.close_particles_list(r2,self.Nlist) #JV: matrix that contains in every row the indexs of the m closest particles
+#            print(self.close_list)
+#            print("")
+#            print(r2)
+#            sys.exit()
 
         for j in range(0,N):
             dUx = 0
@@ -339,7 +380,7 @@ class PhySystem:
 
         self.vel_verlet_on = True #JV: If it's true, it will compute with the velocity verlet algorithm, if it's not, it will compute with normal verlet
 
-        self.Nlist = int(2*(self.particles.size**(1/2))) #JV:This variable defines the number of close particles that will be stored in the list (go to close_particles_list() for more info)
+        self.Nlist = int(2*(self.particles.size)**(1/2)) #JV:This variable defines the number of close particles that will be stored in the list (go to close_particles_list() for more info)
 
         #X,Y,VX,VY has the trajectories of the particles with two indexes that
         #access time and particles, respectively
